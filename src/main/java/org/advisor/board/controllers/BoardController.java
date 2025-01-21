e8062bbfeb622e2171bc4848350cd841c503baaa
SilenceTale <chomn55@gmail.com>
2025-01-21 17:54:20 +0900

    test

---
+++ src/main/java/org/advisor/board/controllers/BoardController.java
@@ -0,0 +1,11 @@
+package org.advisor.board.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class BoardController {
+}
